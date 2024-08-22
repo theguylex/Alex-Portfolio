af3525ba86623f0f990eb15a0b4c515c4b9939c7
Chinonye Ekpendu <alexekpendu001@gmail.com>
2024-08-22 10:41:26 -0800

    menu-icon toggle

---
+++ script.js
@@ -0,0 +1,8 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.addEventListener('click', function(){
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+});
+
