a11d2287ebf4d4266e3bc0907b722c4f12ec4a52
Chinonye Ekpendu <alexekpendu001@gmail.com>
2024-08-28 19:12:48 -0800

    contact me form validation

--- script.js
+++ script.js
@@ -11,3 +11,31 @@ menuIcon.addEventListener('click', function(){
 // document.getElementById('more').addEvenetListener('click', function() {
 //     newAbtTxt.style.display = 'block';
 // });
+
+document.getElementById('registration-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const fullname = document.getElementById('fullName').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const subject = document.getElementById('subject').value.trim();
+    const textarea = document.getElementById('textarea').value.trim();
+    let message = '';
+
+    switch (true) {
+        case !fullname:
+            message = 'Fullname is required.';
+            break;
+        case !email:
+            message = 'Email is required.';
+            break;
+        case !subject:
+            message = 'Subject is required.';
+            break;
+        case !textarea:
+            message = 'Textarea is required.';
+            break;
+        default:
+            message = 'Form submitted successfully!';
+    }
+
+    document.getElementById('validation-message').innerHTML = message;
+}); 
